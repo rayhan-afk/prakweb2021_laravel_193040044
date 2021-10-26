c21fa913d2a69c77cf4ec8469eec08e80effecae
rayhan-afk <58686688+rayhan-afk@users.noreply.github.com>
2021-10-26 23:07:59 +0700

    Menyelesaikan Playlist Laravel Video #11 N+1 Problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-  
+<h1 class="mb-5">{{ $title }}</h1>
+
   @foreach ($posts as $post)
   <article class="mb-5 border-bottom pb-4">
-    <h1>Halaman Blog Posts</h1>
+    
     <h2>
       <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
