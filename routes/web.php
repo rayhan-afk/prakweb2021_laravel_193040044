40c2c7232a7e3e643dde3d663cb9d656e22b2574
rayhan-afk <58686688+rayhan-afk@users.noreply.github.com>
2021-10-27 11:07:45 +0700

    Menyelesaikan Playlist Laravel Video #12 Redesign UI

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', [PostController::class, 'index']);
-
-// halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function () {
     return view('categories', [
-        'title' => "Post Categories",
+        'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -50,6 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
