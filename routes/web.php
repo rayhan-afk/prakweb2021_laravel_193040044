a72de068eb10362d0911281cb77fd0b4faed3813
rayhan-afk <58686688+rayhan-afk@users.noreply.github.com>
2021-11-18 11:35:07 +0700

    Laravel Video #15 User Registration

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,9 @@ Route::get('/categories', function () {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
